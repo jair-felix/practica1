adf8b2fe6949ecf612fc4af1b98b476893ee5174
Jair <chicovirus1@hotmail.com>
2020-10-18 21:13:25 -0500

    avance1

---
+++ Models/formulario.cs
@@ -0,0 +1,30 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace formulario.Models
+{
+    public class formulario
+    {   
+        
+        public String nombre { get; set; }
+
+
+        
+        public String apellido { get; set; }
+
+        public String distrito { get; set; }
+
+        public String Banco { get; set; }
+
+        public int edad { get; set; }
+
+        public String genero { get; set; }
+
+        public String autor { get; set; }
+
+        [Display(Name="Output")]
+        public int Respuesta { get; set; }
+
+    }
+}
